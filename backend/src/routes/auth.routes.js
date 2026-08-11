bca07c3aeb7c6b8f4199b17eca5a75608797ea3d
Kartik Yadav <kartikyadavky1234@gmail.com>
2026-08-11 19:08:09 +0530

    Enhance auth.routes.js with descriptive comments
    Added comments to clarify the purpose of each route.

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,6 +5,7 @@ import { authRequired } from '../middleware/auth.js'
 
 const router = Router()
 
+// if everything is correct then send the request to the signup controller. 
 router.post('/signup', [
   body('email').isEmail(),
   body('password').isLength({ min: 4 }),
@@ -17,11 +18,13 @@ router.post('/signup', [
   next()
 }, signup)
 
+// if everything is correct then login controller ko bhej do 
 router.post('/login', [
   body('email').isEmail(),
   body('password').isLength({ min: 4 })
 ], login)
 
+// check if requestion is authenticated or not if it is then call the profile controller
 router.get('/profile', authRequired, profile)
 
 export default router
